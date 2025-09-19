56f1c25392cf5ca48149c78b34bd83943483c3e8
Ana Luisa <anadrvv@gmail.com>
2025-09-19 11:59:50 -0300

    caminho certo

--- js/menu.js
+++ js/menu.js
@@ -2,10 +2,10 @@ function Menu() {
   return (
     <nav>
       <div className="menu-container">
-        <a href="home.html"><img src="/images/icons/home-icon.png" alt="Home" /></a>
-        <a href="search.html"><img src="/images/icons/search-icon.png" alt="Buscar" /></a>
-        <a href="notifications.html"><img src="/images/icons/notification-icon.png" alt="Notificações" /></a>
-        <a href="profile.html"><img src="/images/icons/profile-icon.png" alt="Perfil" /></a>
+        <a href="./main-page.html"><img src="/images/icons/home-icon.png" alt="Home" /></a>
+        <a href="./items-page.html"><img src="/images/icons/search-icon.png" alt="Buscar" /></a>
+        <a href="./notifications.html"><img src="/images/icons/notification-icon.png" alt="Notificações" /></a>
+        <a href="./user-profile.html"><img src="/images/icons/profile-icon.png" alt="Perfil" /></a>
       </div>
     </nav>
   );
