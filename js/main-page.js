6d85f3b0e03869f295fe7f2e2a465a362c7f2442
lavi <165945697+scriptlver@users.noreply.github.com>
2025-09-20 18:28:24 -0300

    euuu

--- js/main-page.js
+++ js/main-page.js
@@ -7,3 +7,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const filterBtn = document.getElementById("filter-btn");
+const filterMenu = document.querySelector(".filter-menu");
+
+filterBtn.addEventListener("click", (event) => {
+  event.stopPropagation(); 
+  filterMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", (event) => {
+  if (!filterMenu.contains(event.target) && !filterBtn.contains(event.target)) {
+    filterMenu.classList.remove("active");
+  }
+});
+
