7c649b211775b2f50453563f1dc4e07c89b7f052
ash <carolina.goncalves@maisunifacisa.com.br>
2025-09-21 21:32:53 -0300

    pearl

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,21 @@
-//odeioooooo minha vida
+document.querySelector('.btn-send').addEventListener('click', () => {
+  const input = document.querySelector('.chat-input');
+  const message = input.value;
+  
+  if (message.trim() !== '') {
+    //Create a new element
+    const newMessageDiv = document.createElement('div');
+    newMessageDiv.className = 'message sent';
+    newMessageDiv.innerHTML = `<p>${message}</p>`;
+
+    //Add new message
+    document.querySelector('.message-area').appendChild(newMessageDiv);
+
+    //Clean text
+    input.value = '';
+
+    //Last message
+    const messageArea = document.querySelector('.message-area');
+    messageArea.scrollTop = messageArea.scrollHeight;
+  }
+});
