98646403927d0d328ee040092cd2c098a5d94ec1
Ana Luisa <anadrvv@gmail.com>
2025-09-22 10:42:52 -0300

    vamos nos matar

--- js/menu-desktop.js
+++ js/menu-desktop.js
@@ -44,18 +44,3 @@ function MenuDesktop() {
 
 ReactDOM.createRoot(document.getElementById("menu-desktop")).render(<MenuDesktop />);
 
-
-function Menu() {
-  return (
-    <nav>
-      <div className="menu-container">
-        <a href="./main-page.html"><img src="/images/icons/home-icon.png" alt="Home" /></a>
-        <a href="./items-page.html"><img src="/images/icons/search-icon.png" alt="Buscar" /></a>
-        <a href="./notifications.html"><img src="/images/icons/notification-icon.png" alt="Notificações" /></a>
-        <a href="./user-profile.html"><img src="/images/icons/profile-icon.png" alt="Perfil" /></a>
-      </div>
-    </nav>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("menu")).render(<Menu />);
--- js/menu.js
+++ js/menu.js
@@ -2,13 +2,27 @@ function Menu() {
   return (
     <nav className="nav-mobile">
       <div className="menu-container">
-        <a href="./main-page.html"><img src="/images/icons/home-icon.png" alt="Home" /></a>
-        <a href="./main-page.html"><img src="/images/icons/search-icon.png" alt="Buscar" /></a>
-        <a href="./notifications.html"><img src="/images/icons/notification-icon.png" alt="Notificações" /></a>
-        <a href="./main-profile.html"><img src="/images/icons/icon-menu-ab.png" alt="Perfil" /></a>
+        <a href="./main-page.html">
+          <img src="../images/icons/home-icon.png" alt="Home" />
+        </a>
+        <a href="./main-page.html">
+          <img src="../images/icons/search-icon.png" alt="Buscar" />
+        </a>
+        <a href="./notifications.html">
+          <img src="../images/icons/notification-icon.png" alt="Notificações" />
+        </a>
+        <a href="./main-profile.html">
+          <img src="../images/icons/profile-icon.png" alt="Perfil" />
+        </a>
       </div>
     </nav>
   );
 }
 
-ReactDOM.createRoot(document.getElementById("menu")).render(<Menu />);
+
+const menuRoot = document.getElementById("menu");
+if (menuRoot) {
+  ReactDOM.createRoot(menuRoot).render(<Menu />);
+}
+
+
