a2139990ec164359f806977bd9315f43c67322c3
Ana Luisa <anadrvv@gmail.com>
2025-09-22 18:15:55 -0300

    Murder Song (5, 4, 3, 2, 1)

--- js/main-page.js
+++ js/main-page.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach(card => {
-    card.addEventListener("click", () => {
-      window.location.href = "/pages/item-detail.html"; 
-    });
-  });
-});
 
 const filterBtn = document.getElementById("filter-btn");
 const filterMenu = document.querySelector(".filter-menu");
